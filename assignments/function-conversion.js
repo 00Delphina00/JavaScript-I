af87ae2ccaaab388d284191685363736fcf77124
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-04-18 01:23:01 -0400

    function-conversion.js stretch exercise.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -27,12 +27,18 @@ console.log(add(1, 2));
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
+let subtract = (param1, param2) =>{
+    return param1 - param2;
+}
+console.log(subtract(1,2));
 // Stretch
 
-// exampleArray = [1,2,3,4];
+ let exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+const triple = exampleArray.map((num)=>{ 
+    return num * 3;
+});
+console.log("This is triple " + triple);
